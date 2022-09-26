d2f45cf89402d3d064f2c677099763019f90cf30
Maiada Ibrahim <m.Ibrahim@lgcloud.jo>
2022-09-26 11:47:05 +0300

    PUSHTICKET

--- JepcoBackEndSystemProject.MeterReaderCompanyServices/Controllers/FaultCompliantsControllers.cs
+++ JepcoBackEndSystemProject.MeterReaderCompanyServices/Controllers/FaultCompliantsControllers.cs
@@ -253,7 +253,7 @@ namespace JepcoBackEndSystemProject.EmergancyAppApis.Controllers
                 var lstFalutComplaint = await _repository.FaultCompliantsLookupRepository.GetListOfFaultCompliants(x => x.UserID == FaultComplaintDto.UserID && x.CompliantParentRefNumber == FaultComplaintDto.ComplaintRefCode &&   x.FaultStatusID != 4).ConfigureAwait(false);
 
 
-                return Ok(_common.ReturnOkData(_common.ReturnResourceValue(_localizerAR, _localizerEN, FaultComplaintDto.LanguageId, "Returned all Fault Complaints fro Technication"), lstFalutComplaint));
+                return Ok(_common.ReturnOkData(_common.ReturnResourceValue(_localizerAR, _localizerEN, FaultComplaintDto.LanguageId, "Returned NEW Fault Complaints fro Technication"), lstFalutComplaint));
 
             }
             catch (Exception ex)
@@ -269,21 +269,18 @@ namespace JepcoBackEndSystemProject.EmergancyAppApis.Controllers
         //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [HttpPost(Name = "PushTicket")]
         [Route("PushTicket")]
-        public async Task<ActionResult<CommonReturnResult>> PushTicket([FromBody] PushRequestDTO PushRequest)
+        public async Task<ActionResult<CommonReturnResult>> PushTicket([FromBody] List<PushRequestDTO> PushRequest)
         {
 
 
             try
             {
 
-                MenaTrackService.CallCenterNewClient objCallCenterNewClient = new MenaTrackService.CallCenterNewClient(MenaTrackService.CallCenterNewClient.EndpointConfiguration.BasicHttpsBinding_ICallCenterNew);
-
-                List<MenaTrackService.JEPCOViewRequestsParentsResponse> lstJEPCOViewRequestsParentsResponse = await objCallCenterNewClient.JEPCO_ViewRequests_ParentsAsync(PushRequest.UserID, PushRequest.BranchID).ConfigureAwait(false);
 
 
-                if (lstJEPCOViewRequestsParentsResponse != null && lstJEPCOViewRequestsParentsResponse.Count > 0)
+                if (PushRequest != null && PushRequest.Count > 0)
                 {
-                    foreach (var jEPCOViewRequest in lstJEPCOViewRequestsParentsResponse)
+                    foreach (var jEPCOViewRequest in PushRequest)
                     {
 
 
@@ -293,39 +290,40 @@ namespace JepcoBackEndSystemProject.EmergancyAppApis.Controllers
                         {
 
                             tb_Fault_Compliants tb_Fault_Compliants = new tb_Fault_Compliants();
+
                             tb_Fault_Compliants.ComplaintDescription = jEPCOViewRequest.ComplaintDescription;
-                            tb_Fault_Compliants.ComplaintRefNumber = jEPCOViewRequest.Refcode;
+                            tb_Fault_Compliants.ComplaintRefNumber = jEPCOViewRequest.ComplaintRefNumber;
                             tb_Fault_Compliants.ComplaintDescription = jEPCOViewRequest.ComplaintDescription.ToString();
-                            tb_Fault_Compliants.CompliantCustomerName = jEPCOViewRequest.CustomerName;
-                            tb_Fault_Compliants.CompliantDateTime = jEPCOViewRequest.ComplaintDate;
-                            tb_Fault_Compliants.CompliantPhoneNumber = jEPCOViewRequest.CustomerMobileNumber;
+                            tb_Fault_Compliants.CompliantCustomerName = jEPCOViewRequest.CompliantCustomerName;
+                            tb_Fault_Compliants.CompliantDateTime = jEPCOViewRequest.CompliantDateTime;
+                            tb_Fault_Compliants.CompliantPhoneNumber = jEPCOViewRequest.CompliantPhoneNumber;
                             tb_Fault_Compliants.SubstationNumber = jEPCOViewRequest.SubstationNumber;
-                            tb_Fault_Compliants.CompliantParentRefNumber = jEPCOViewRequest.ParentRefcode;
-                            tb_Fault_Compliants.CustomerAddress_Latt = jEPCOViewRequest.AddressLatt;
-                            tb_Fault_Compliants.CustomerAddress_Long = jEPCOViewRequest.AddressLong;
+                            tb_Fault_Compliants.CompliantParentRefNumber = jEPCOViewRequest.CompliantParentRefNumber;
+                            tb_Fault_Compliants.CustomerAddress_Latt = jEPCOViewRequest.CustomerAddress_Latt;
+                            tb_Fault_Compliants.CustomerAddress_Long = jEPCOViewRequest.CustomerAddress_Long;
 
                             tb_Fault_Compliants.DistrictID = jEPCOViewRequest.DistrictID;
-                            tb_Fault_Compliants.DistrictName = jEPCOViewRequest.District;
-                            tb_Fault_Compliants.GovernateId = jEPCOViewRequest.GovernateID;
-                            tb_Fault_Compliants.GovernateName = jEPCOViewRequest.Governate;
+                            tb_Fault_Compliants.DistrictName = jEPCOViewRequest.DistrictName;
+                            tb_Fault_Compliants.GovernateId = jEPCOViewRequest.GovernateId;
+                            tb_Fault_Compliants.GovernateName = jEPCOViewRequest.GovernateName;
                             tb_Fault_Compliants.ZoneID = jEPCOViewRequest.ZoneID;
                             tb_Fault_Compliants.ZoneName = jEPCOViewRequest.ZoneName;
 
                             tb_Fault_Compliants.StreetID = jEPCOViewRequest.StreetID;
-                            tb_Fault_Compliants.StreetName = jEPCOViewRequest.Street;
+                            tb_Fault_Compliants.StreetName = jEPCOViewRequest.ZoneName;
                             tb_Fault_Compliants.FaultStatusID = 1;
-                            tb_Fault_Compliants.MenaTrackStatusDesc = jEPCOViewRequest.StatusDesc;
-                            tb_Fault_Compliants.MenaTrackStatusID = jEPCOViewRequest.StatusID;
+                            tb_Fault_Compliants.MenaTrackStatusDesc = jEPCOViewRequest.MenaTrackStatusDesc;
+                            tb_Fault_Compliants.MenaTrackStatusID = jEPCOViewRequest.MenaTrackStatusID;
                             tb_Fault_Compliants.IssueID = jEPCOViewRequest.IssueID;
-                            tb_Fault_Compliants.PiorityID = jEPCOViewRequest.PriorityID;
-                            tb_Fault_Compliants.PiorityDesc = jEPCOViewRequest.PriorityDesc;
-                            tb_Fault_Compliants.UserID = PushRequest.UserID;
-                            tb_Fault_Compliants.UserName = PushRequest.UserName;
-                            tb_Fault_Compliants.BranchID = PushRequest.BranchID;
+                            tb_Fault_Compliants.PiorityID = jEPCOViewRequest.PiorityID;
+                            tb_Fault_Compliants.PiorityDesc = jEPCOViewRequest.PiorityDesc;
+                            tb_Fault_Compliants.UserID = jEPCOViewRequest.UserID;
+                            tb_Fault_Compliants.UserName = jEPCOViewRequest.UserName;
+                            tb_Fault_Compliants.BranchID = jEPCOViewRequest.BranchID;
                             tb_Fault_Compliants.CreatedDate = DateTime.Now;
                             tb_Fault_Compliants.UpdateDate = DateTime.Now;
-                            tb_Fault_Compliants.LV_Feeder = jEPCOViewRequest.LVFeederNumber;
-                            tb_Fault_Compliants.MV_Feeder = jEPCOViewRequest.MVFeederNumber;
+                            tb_Fault_Compliants.LV_Feeder = jEPCOViewRequest.LV_Feeder;
+                            tb_Fault_Compliants.MV_Feeder = jEPCOViewRequest.MV_Feeder;
                             tb_Fault_Compliants.SubstationNumber = jEPCOViewRequest.SubstationNumber;
                             //tb_Fault_Compliants.SubstationName  = jEPCOViewRequest.s;
 
@@ -361,16 +359,16 @@ namespace JepcoBackEndSystemProject.EmergancyAppApis.Controllers
 
 
 
-                var lstFalutComplaint = await _repository.FaultCompliantsLookupRepository.GetListOfFaultCompliants(x => x.UserID == FaultComplaintDto.UserID && x.FaultStatusID != 4).ConfigureAwait(false);
+                var lstFalutComplaint = await _repository.FaultCompliantsLookupRepository.GetListOfFaultCompliants(x => x.UserID == PushRequest.UserID && x.FaultStatusID != 4).ConfigureAwait(false);
 
 
-                return Ok(_common.ReturnOkData(_common.ReturnResourceValue(_localizerAR, _localizerEN, FaultComplaintDto.LanguageId, "Returned all Fault Complaints fro Technication"), lstFalutComplaint));
+                return Ok(_common.ReturnOkData(_common.ReturnResourceValue(_localizerAR, _localizerEN, "AR", "Save all new Fault Complaints for Technication"), lstFalutComplaint));
 
             }
             catch (Exception ex)
             {
                 _logger.LogError($"Something went wrong inside GetParentFaultCompliantList action: {ex.Message + System.Environment.NewLine + ex.InnerException + ex.StackTrace}");
-                return BadRequest(_common.ReturnBadData(_common.ReturnResourceValue(_localizerAR, _localizerEN, FaultComplaintDto.LanguageId, "Error"), _common.ReturnResourceValue(_localizerAR, _localizerEN, FaultComplaintDto.LanguageId, "Internal server error")));
+                return BadRequest(_common.ReturnBadData(_common.ReturnResourceValue(_localizerAR, _localizerEN, "AR", "Error"), _common.ReturnResourceValue(_localizerAR, _localizerEN, FaultComplaintDto.LanguageId, "Internal server error")));
             }
 
         }
