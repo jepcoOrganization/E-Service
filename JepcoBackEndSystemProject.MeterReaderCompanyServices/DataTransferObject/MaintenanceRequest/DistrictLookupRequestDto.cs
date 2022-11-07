a190136ee523b1f4332824a493f898924c67d41b
Maiada Ibrahim <m.Ibrahim@lgcloud.jo>
2022-11-07 17:05:57 +0200

    insert

---
+++ JepcoBackEndSystemProject.MeterReaderCompanyServices/DataTransferObject/MaintenanceRequest/DistrictLookupRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JepcoBackEndSystemProject.EService.DataTransferObject.MaintenanceRequest
+{
+    public class DistrictLookupRequestDto
+{
+        [Required]
+        public string LanguageId { get; set; }
+
+        [Required]
+        public long GovernateId { get; set; }
+
+    }
+}
